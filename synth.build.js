d83549c74a484239d9094333b2915bff4ca443d9
Aneesh Durg <aneeshdurg17@gmail.com>
2021-01-15 21:57:22 -0800

    Updates

--- synth.build.js
+++ synth.build.js
@@ -22,7 +22,7 @@ async function getFile(url) {
 
     let fileContents = "";
     const reader = resp.body.getReader();
-    done = false;
+    let done = false;
     while (!done) {
         let fileBody = await reader.read();
         if (!fileBody.value) {
