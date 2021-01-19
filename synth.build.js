9829fa9c7efac3bd4b6360491d695f9eed0c908a
Aneesh Durg <aneeshdurg17@gmail.com>
2021-01-18 22:29:44 -0800

    Updates

--- synth.build.js
+++ synth.build.js
@@ -1,3 +1,7 @@
+// ---------- lz-string/libs/lz-string.min.js ----------
+var LZString=function(){function o(o,r){if(!t[o]){t[o]={};for(var n=0;n<o.length;n++)t[o][o.charAt(n)]=n}return t[o][r]}var r=String.fromCharCode,n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",t={},i={compressToBase64:function(o){if(null==o)return"";var r=i._compress(o,6,function(o){return n.charAt(o)});switch(r.length%4){default:case 0:return r;case 1:return r+"===";case 2:return r+"==";case 3:return r+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(e){return o(n,r.charAt(e))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(o){return null==o?"":""==o?null:i._decompress(o.length,16384,function(r){return o.charCodeAt(r)-32})},compressToUint8Array:function(o){for(var r=i.compress(o),n=new Uint8Array(2*r.length),e=0,t=r.length;t>e;e++){var s=r.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null===o||void 0===o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;t>e;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(o){return null==o?"":i._compress(o,6,function(o){return e.charAt(o)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(n){return o(e,r.charAt(n))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")},decompress:function(o){return null==o?"":""==o?null:i._decompress(o.length,32768,function(r){return o.charCodeAt(r)})},_decompress:function(o,n,e){var t,i,s,p,u,c,a,l,f=[],h=4,d=4,m=3,v="",w=[],A={val:e(0),position:n,index:1};for(i=0;3>i;i+=1)f[i]=i;for(p=0,c=Math.pow(2,2),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(t=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 2:return""}for(f[3]=l,s=l,w.push(l);;){if(A.index>o)return"";for(p=0,c=Math.pow(2,m),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(l=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 2:return w.join("")}if(0==h&&(h=Math.pow(2,m),m++),f[l])v=f[l];else{if(l!==d)return null;v=s+s.charAt(0)}w.push(v),f[d++]=s+v.charAt(0),h--,s=v,0==h&&(h=Math.pow(2,m),m++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module&&(module.exports=LZString);
+// ---------- END lz-string/libs/lz-string.min.js ------
+
 // ---------- webgl-common/common.js ----------
 async function loadTwgl() {
     const p = new Promise((resolve) => {
@@ -2732,7 +2736,17 @@ function load_meta_modules(moduledata_descs) {
         register_module(module_name, moduledata_descs[module_name]);
 }
 
+const MAGIC = "SYN".split('').map(x => x.charCodeAt(0));
+MAGIC.push(255);
+const header = MAGIC.length + 4;
 function setup_save_load(ui, synth) {
+    // magic + 4 byte length + 1 byte per RGBA values
+    // this is because we can't use the A channel because of premultiplied
+    // stuff, TODO fix that
+    const max_stego_size = Math.min(
+        0xffffff,
+        (4 * synth.dimensions[0] * synth.dimensions[1] - header) / 4);
+
     document.getElementById("save").addEventListener('click', () => {
         const saved = [];
         for (let i = 0; i < ui.children.length; i++) {
@@ -2744,16 +2758,68 @@ function setup_save_load(ui, synth) {
             modules: meta_modules,
         };
 
-        const savedata = encodeURI(JSON.stringify(saveobj));
-        const downloader = document.createElement('a');
-        downloader.setAttribute('href', 'data:text/plain;charset=utf-8,' + savedata);
-        downloader.setAttribute('download', 'videoSynth.savedata');
-        downloader.style.display = "none";
-        document.body.appendChild(downloader);
+        const savestr = JSON.stringify(saveobj);
 
-        downloader.click();
+        const compressed = LZString.compressToUint8Array(savestr)
+        console.log(compressed.length);
+        console.log(compressed);
+        const stego_possible = compressed.length < max_stego_size && compressed.length < 0xffffffff;
+        console.log("sp", stego_possible);
+        if (stego_possible) {
+            const output_canvas = document.createElement("canvas");
+            output_canvas.width = synth.dimensions[0];
+            output_canvas.height = synth.dimensions[1];
 
-        document.body.removeChild(downloader);
+            const output_ctx = output_canvas.getContext("2d");
+            const img = output_ctx.createImageData(...synth.dimensions);
+            synth.get_frame_data(img.data);
+
+            console.log("encoding MAGIC");
+            for (let i = 0; i < MAGIC.length; i++)
+                img.data[i] = MAGIC[i];
+
+            console.log("encoding len");
+            let length = compressed.length;
+            for (let i = 0; i < 3; i++) {
+                img.data[MAGIC.length + i] = length % 256;
+                length = Math.floor(length / 256);
+            }
+
+
+            console.log("encoding data");
+            for (let i = 0; i < compressed.length; i++) {
+                const idx = i * 4;
+                img.data[header + idx + 0] &= 0xf0;
+                img.data[header + idx + 0] += (0xf0 & compressed[i]) / 16;
+
+                img.data[header + idx + 1] &= 0xf0;
+                img.data[header + idx + 1] += 0x0f & compressed[i];
+            }
+
+            output_ctx.putImageData(img, 0, 0);
+            const out_data = output_ctx.canvas.toDataURL();
+
+            const downloader = document.createElement('a');
+            downloader.setAttribute('href', out_data);
+            downloader.setAttribute('download', 'synth.savedata.png');
+            downloader.style.display = "none";
+            document.body.appendChild(downloader);
+
+            downloader.click();
+
+            document.body.removeChild(downloader);
+        } else {
+            const savedata = encodeURI(savestr);
+            const downloader = document.createElement('a');
+            downloader.setAttribute('href', 'data:text/plain;charset=utf-8,' + savedata);
+            downloader.setAttribute('download', 'videoSynth.savedata');
+            downloader.style.display = "none";
+            document.body.appendChild(downloader);
+
+            downloader.click();
+
+            document.body.removeChild(downloader);
+        }
     });
 
     const loadUpload = document.getElementById("load");
@@ -2761,16 +2827,76 @@ function setup_save_load(ui, synth) {
         let file = loadUpload.files[0];
         let reader = new FileReader();
         console.log(file, reader);
-        reader.readAsText(file)
-        reader.onloadend = () => {
-            const savedata = JSON.parse(reader.result);
-            if (savedata.stages) {
+        if (file.name.endsWith(".png")) {
+            reader.readAsDataURL(file)
+            reader.onloadend = async () => {
+                const img = new Image();
+                img.src = reader.result;
+                await new Promise(r => { img.onload = r; });
+                console.log(img);
+
+                const canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+
+                const ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+
+                const ctxdata = ctx.getImageData(0, 0, ...synth.dimensions);
+                const stegodata = ctxdata.data;
+                console.log(stegodata);
+                // const stegodata = new Uint8Array(reader.result);
+                for (let i = 0; i < MAGIC.length; i++) {
+                    if (stegodata[i] != MAGIC[i]) {
+                        console.log(stegodata);
+                        throw new Error("File is not synth data");
+                    }
+                }
+
+                let length = 0;
+                for (let i = 2; i >= 0; i--) {
+                    length *= 256;
+                    const newdata = stegodata[MAGIC.length + i];
+                    console.log(length, '*', 256, '+', newdata);
+                    length += newdata;
+                }
+
+                console.log("len", length);
+                const data = new Uint8Array(length);
+                for (let i = 0; i < length; i++) {
+                    const idx = 4 * i;
+                    if (i == 0) {
+                        console.log(stegodata[header + idx + 0]);
+                        console.log(stegodata[header + idx + 0] & 0x0f);
+                        console.log(stegodata[header + idx + 1]);
+                        console.log(stegodata[header + idx + 1] & 0x0f);
+                    }
+                    const entry = (stegodata[header + idx + 0] & 0x0f) * 16 +
+                                    (stegodata[header + idx + 1] & 0x0f);
+                    data[i] = entry;
+                }
+
+                console.log(data);
+
+                const result = LZString.decompressFromUint8Array(data);
+                console.log(result);
+
+                const savedata = JSON.parse(result);
                 load_meta_modules(savedata.modules, ui, synth);
                 loaddata(savedata.stages, ui, synth);
-            } else { // older version for compat
-                loaddata(savedata, ui, synth);
             }
-        };
+        } else {
+            reader.readAsText(file)
+            reader.onloadend = () => {
+                const savedata = JSON.parse(reader.result);
+                if (savedata.stages) {
+                    load_meta_modules(savedata.modules, ui, synth);
+                    loaddata(savedata.stages, ui, synth);
+                } else { // older version for compat
+                    loaddata(savedata, ui, synth);
+                }
+            };
+        }
     });
 }
 // ---------- END saveload.js ------
@@ -2884,6 +3010,10 @@ class Synth {
             this.record_frames--;
         }
 
+    }
+
+    get_frame_data(array) {
+        this.gl.readPixels(0, 0, ...this.dimensions, this.gl.RGBA, this.gl.UNSIGNED_BYTE, array);
     }
 
     add_stage(name, module) {
